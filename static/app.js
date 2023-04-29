85ffc2c8847ef0b36d05fbacec27cf365431d1f8
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-29 19:35:11 +0530

    Running Fine -  29-04-23 07:34 PM

--- static/app.js
+++ static/app.js
@@ -1,11 +1,11 @@
-class ChatBox{
+class Chatbox{
     constructor(){
         this.args={
             openButton:document.querySelector('.chatbox__button'),
             chatBox:document.querySelector('.chatbox__support'),
             sendButton:document.querySelector('.send__button')
         }
-        this.state=true;
+        this.state=false;
         this.messages=[];
     }
     display(){
@@ -58,15 +58,15 @@ class ChatBox{
         var html='';
         this.messages.slice().reverse().forEach(function(item,index){
             if(item.name==="Arin"){
-                html+='<div class="messages__item messages__item--visitor'>+item.message+'</div>'
+                html+='<div class="messages__item messages__item--visitor">' +item.message+'</div>'
             }
             else{
-                html+='<div class="messages__item messages__item--operator">'+item.name+': '+item.message+'</div>'
+                html+='<div class="messages__item messages__item--operator">'+item.message+'</div>'
             }
         });
         const chatmessage=chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML=html;
     }
 }
-const chatbox=new ChatBox();
+const chatbox=new Chatbox();
 chatbox.display();
