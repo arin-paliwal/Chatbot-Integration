37db9f462618bcccba7b5eeeed8a395b2f794e7d
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-29 19:57:53 +0530

    chatbot running fine 07:57 29-04-23

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS # for integrating it with other every website
 from chat import get_response
 app = Flask(__name__)
+CORS(app) # for linking
 
-
+# not needed -- for cors
 @app.get("/")  # flask 2.0
 def index_get():
     return render_template('base.html')
